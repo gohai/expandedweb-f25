11a5089685077bec9634f4c1e891bf1d9e285074
gohai <gottfried.haider@gmail.com>
2025-10-21 20:09:13 +0800

    Add additional details how to get the stream data into p5.js

--- 02_node/16_node_webcam_mjpeg/public/sketch.js
+++ 02_node/16_node_webcam_mjpeg/public/sketch.js
@@ -1,9 +1,18 @@
 // Expanded Web
 // NYUSH F25 - gohai
 
+let streamEl;
+
 function setup() {
   noCanvas();
+
+  // if we want to use the live stream image within p5
+  // we can do this:
+  streamEl = select('#stream');
+  // optionally hide the source element (like with webcam):
+  //streamEl.hide();
 }
 
 function draw() {
+  //image(streamEl, 0, 0);
 }
